bb7702c3c6515a369ffddd6d8af70d0b36c68734
Clément Richard <clementrichard@macbook-pro-de-clement.home>
2023-11-18 16:20:56 +0100

    replace SHA256

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 // const Offer = require("../models/Offer");
 const User = require("../models/User");
 const uid2 = require("uid2");
-const SHA256 = require("crypto-js/sha56");
+const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 
 router.post("/user/signup", async (req, res) => {
