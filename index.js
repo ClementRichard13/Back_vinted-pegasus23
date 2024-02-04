a20ffd688b9c4cdcebc40e80b0fb1eebd846f3d6
Clément Richard <clementrichard@macbook-pro-de-clement.home>
2024-02-04 12:24:06 +0100

    oui

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 });
-
+// oui
 const userRoute = require("./routes/user");
 app.use(userRoute);
 const offerRoutes = require("./routes/offer");
